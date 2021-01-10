c7704b299ecb9dc9217d59f0ca241f94f3da9306
Sangwon Ryu <bdboy090@naver.com>
2021-01-10 16:55:53 +0900

    Add Navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="https://hangul.sryu.dev/">hangul.sryu.dev</a>
+        <a role="button" class="navbar-burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            
+        </div>
+        <div class="navbar-end">
+            <a class="navbar-item" href="https://github.com/sangwon090/hangul.sryu.dev/"><i class="fab fa-github"></i>&nbsp;GitHub</a>
+        </div>
+    </div>
+</nav>
